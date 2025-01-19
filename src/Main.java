de9d0c8fd3a2e8c0748f5adedd23a57233a00aca
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-01-19 22:11:04 +0000

    Add one stack

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,10 @@ public class Main {
     public static void main(String[] args) {
         Stack<String> stack = new Stack<String>();
 
+//        System.out.println(stack.empty());
+
+        stack.push("Minecraft");
+
+
     }
 }
